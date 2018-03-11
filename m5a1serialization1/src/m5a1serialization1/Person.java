8b8db48320a123e391a95ddfdefa7376671d7269
scruffpup <scruffpup@gmail.com>
2018-03-11 16:25:25 -0600

    first one

---
+++ m5a1serialization1/src/m5a1serialization1/Person.java
@@ -0,0 +1,82 @@
+/*
+ *
+ *  Project    :  m5a1serialization1
+ *  Assignment :
+ *  File       :  Person.java
+ *  Name       :  Kelly Tidwell
+ *  Date       :  Mar 11, 2018
+ *
+ *  Description : (Narrative desciption, not code)
+ *
+ *		a Person class with name and cell phone number
+ *
+ ********************************************************/
+
+package m5a1serialization1;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	private String firstName;
+	private String lastName;
+	
+	private String cellPhone;
+	
+	public Person(String fn, String ln, String phone)
+	{
+		this.setFirstName(fn);
+		this.setLastName(ln);
+		this.setCellPhone(phone);
+		
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the cellPhone
+	 */
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	/**
+	 * @param cellPhone the cellPhone to set
+	 */
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return (String.format("%s %s %s", this.getFirstName(), this.getLastName(), this.getCellPhone()));
+	}
+	
+
+}
---
+++ m5a1serialization1/src/m5a1serialization1/SerializationApp.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  Project    :  m5a1serialization1
+ *  Assignment :
+ *  File       :  SerializationApp.java
+ *  Name       :  Kelly Tidwell
+ *  Date       :  Mar 11, 2018
+ *
+ *  Description : (Narrative desciption, not code)
+ *		
+ *
+ ********************************************************/
+
+package m5a1serialization1;
+
+public class SerializationApp {
+
+	public static void main(String[] args) {
+		/****************************************************
+		 * Method     : main
+		 *
+		 * Purpose    : 
+		 *
+		 * Parameters : 
+		 *
+		 * Returns    : 
+		 *
+		 ****************************************************/
+		// TODO Auto-generated method stub
+
+	}
+
+}
